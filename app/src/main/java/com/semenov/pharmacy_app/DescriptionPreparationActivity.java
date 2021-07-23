7c2c34d521c714d3255b744a76e740e1034c1003
Nikolui333 <semenovnikolu@yandex.ru>
2021-07-23 22:03:15 +0300

    PreparationEnd

--- app/src/main/java/com/semenov/pharmacy_app/DescriptionPreparationActivity.java
+++ app/src/main/java/com/semenov/pharmacy_app/DescriptionPreparationActivity.java
@@ -2,13 +2,33 @@ package com.semenov.pharmacy_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class DescriptionPreparationActivity extends AppCompatActivity {
+
+    TextView textView;
+    TextView textView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_preparation);
+
+        textView = (TextView) findViewById(R.id.name);
+        textView2 = (TextView) findViewById(R.id.UpText);
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("name");
+        String text = intent.getStringExtra("text");
+
+        textView.setText(name);
+        textView2.setText(text);
+
+        textView2.setMovementMethod(new ScrollingMovementMethod());
     }
 }
